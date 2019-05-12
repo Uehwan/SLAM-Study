bee96f6647d98ab4494ea9ebb2997cdfc181e2f4
Uehwan <ohnefetter@kaist.ac.kr>
2019-05-12 23:01:55 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,310 @@
+import argparse
+import time
+import csv
+
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import custom_transforms
+from disvo_model import DISVO
+from disvo_data import KITTIDataset
+from utils import tensor2array, save_checkpoint, save_path_formatter, log_output_tensorboard
+
+from logger import TermLogger, AverageMeter
+from tensorboardX import SummaryWriter
+
+parser = argparse.ArgumentParser(description='Simultaneous Detection, Instance Segmentation and Visual Odometry',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('data', metavar='DIR',
+                    help='path to dataset')
+parser.add_argument('--max-distance', type=int, metavar='N', help='maximum distance between reference and target', default=15)
+parser.add_argument('--rotation-mode', type=str, choices=['euler', 'quat'], default='euler',
+                    help='rotation mode for PoseExpnet : euler (yaw,pitch,roll) or quaternion (last 3 coefficients)')
+parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                    help='number of data loading workers')
+parser.add_argument('--epochs', default=200, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--epoch-size', default=0, type=int, metavar='N',
+                    help='manual epoch size (will match dataset size if not set)')
+parser.add_argument('-b', '--batch-size', default=4, type=int,
+                    metavar='N', help='mini-batch size')
+parser.add_argument('--lr', '--learning-rate', default=2e-4, type=float,
+                    metavar='LR', help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum for sgd, alpha parameter for adam')
+parser.add_argument('--beta', default=0.999, type=float, metavar='M',
+                    help='beta parameters for adam')
+parser.add_argument('--weight-decay', '--wd', default=0, type=float,
+                    metavar='W', help='weight decay')
+parser.add_argument('--print-freq', default=10, type=int,
+                    metavar='N', help='print frequency')
+parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                    help='evaluate model on validation set')
+parser.add_argument('--pretrained-disvo', dest='pretrained_disvo', default=None, metavar='PATH',
+                    help='path to pre-trained disvo model')
+parser.add_argument('--seed', default=0, type=int, help='seed for random functions, and network initialization')
+parser.add_argument('--log-summary', default='progress_log_summary.csv', metavar='PATH',
+                    help='csv where to save per-epoch train and valid stats')
+parser.add_argument('--log-full', default='progress_log_full.csv', metavar='PATH',
+                    help='csv where to save per-gradient descent train stats')
+
+best_error = -1
+n_iter = 0
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+
+def main():
+    global best_error, n_iter, device
+    args = parser.parse_args()
+    save_path = save_path_formatter(args, parser)
+    args.save_path = 'checkpoints'/save_path
+    print('=> will save everything to {}'.format(args.save_path))
+    args.save_path.makedirs_p()
+    torch.manual_seed(args.seed)
+    if args.evaluate:
+        args.epochs = 0
+
+    training_writer = SummaryWriter(args.save_path)
+    output_writers = []
+    if args.log_output:
+        for i in range(3):
+            output_writers.append(SummaryWriter(args.save_path/'valid'/str(i)))
+
+    # Data loading code
+    train_transform = custom_transforms.Compose([
+        custom_transforms.RandomHorizontalFlip(),
+        custom_transforms.RandomScaleCrop(),
+        custom_transforms.ArrayToTensor()
+    ])
+
+    valid_transform = custom_transforms.Compose([custom_transforms.ArrayToTensor(), normalize])
+
+    print("=> fetching scenes in '{}'".format(args.data))
+    train_set = KITTIDataset(
+        root_dir,
+        sequences,
+        max_distance=args.max_distance,
+        transform=None
+    )
+
+    val_set = KITTIDataset(
+        root_dir,
+        sequences,
+        max_distance=args.max_distance,
+        transform=None
+    )
+    print('{} samples found in {} train scenes'.format(len(train_set), len(train_set)))
+    print('{} samples found in {} valid scenes'.format(len(val_set), len(val_set)))
+
+    train_loader = torch.utils.data.DataLoader(
+        train_set, batch_size=args.batch_size, shuffle=True,
+        num_workers=args.workers, pin_memory=True)
+    val_loader = torch.utils.data.DataLoader(
+        val_set, batch_size=args.batch_size, shuffle=False,
+        num_workers=args.workers, pin_memory=True)
+
+    if args.epoch_size == 0:
+        args.epoch_size = len(train_loader)
+
+    # create model
+    print("=> creating model")
+
+    config_file = "./configs/e2e_mask_rcnn_R_50_FPN_1x.yaml"
+    cfg.merge_from_file(config_file)
+    cfg.freeze()
+    pretrained_model_path = "./e2e_mask_rcnn_R_50_FPN_1x.pth"
+    disvo = DISVO(cfg, pretrained_model_path).cuda()
+
+    if args.pretrained_disvo:
+        print("=> using pre-trained weights for Dispnet")
+        weights = torch.load(args.pretrained_disvo)
+        disvo.load_state_dict(weights['state_dict'])
+    else:
+        disvo.init_weights()
+
+    cudnn.benchmark = True
+
+    print('=> setting adam solver')
+
+    optim_params = [
+        {'params': disvo.parameters(), 'lr': args.lr}
+    ]
+    optimizer = torch.optim.Adam(optim_params,
+                                 betas=(args.momentum, args.beta),
+                                 weight_decay=args.weight_decay)
+
+    with open(args.save_path/args.log_summary, 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter='\t')
+        writer.writerow(['train_loss', 'validation_loss'])
+
+    with open(args.save_path/args.log_full, 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter='\t')
+        writer.writerow(['train_loss'])
+
+    logger = TermLogger(n_epochs=args.epochs, train_size=min(len(train_loader), args.epoch_size), valid_size=len(val_loader))
+    logger.epoch_bar.start()
+
+    if args.pretrained_disvo or args.evaluate:
+        logger.reset_valid_bar()
+        errors, error_names = validate(args, val_loader, disvo, 0, logger, output_writers)
+        for error, name in zip(errors, error_names):
+            training_writer.add_scalar(name, error, 0)
+        error_string = ', '.join('{} : {:.3f}'.format(name, error) for name, error in zip(error_names[2:9], errors[2:9]))
+        logger.valid_writer.write(' * Avg {}'.format(error_string))
+
+    for epoch in range(args.epochs):
+        logger.epoch_bar.update(epoch)
+
+        # train for one epoch
+        logger.reset_train_bar()
+        train_loss = train(args, train_loader, disvo, optimizer, args.epoch_size, logger, training_writer)
+        logger.train_writer.write(' * Avg Loss : {:.3f}'.format(train_loss))
+
+        # evaluate on validation set
+        logger.reset_valid_bar()
+        errors, error_names = validate(args, val_loader, disvo, 0, logger, output_writers)
+        error_string = ', '.join('{} : {:.3f}'.format(name, error) for name, error in zip(error_names, errors))
+        logger.valid_writer.write(' * Avg {}'.format(error_string))
+
+        for error, name in zip(errors, error_names):
+            training_writer.add_scalar(name, error, epoch)
+
+        # Up to you to chose the most relevant error to measure your model's performance, careful some measures are to maximize (such as a1,a2,a3)
+        decisive_error = errors[1]
+        if best_error < 0:
+            best_error = decisive_error
+
+        # remember lowest error and save checkpoint
+        is_best = decisive_error < best_error
+        best_error = min(best_error, decisive_error)
+        save_checkpoint(
+            args.save_path, {
+                'epoch': epoch + 1,
+                'state_dict': disvo.module.state_dict()
+            },
+            is_best)
+
+        with open(args.save_path/args.log_summary, 'a') as csvfile:
+            writer = csv.writer(csvfile, delimiter='\t')
+            writer.writerow([train_loss, decisive_error])
+    logger.epoch_bar.finish()
+
+
+def train(args, train_loader, disvo, optimizer, epoch_size, logger, train_writer):
+    global n_iter, device
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter(precision=4)
+
+    # switch to train mode
+    disvo.train()
+
+    end = time.time()
+    logger.train_bar.update(0)
+
+    for i, (img_ref, img_tar, poses_gt) in enumerate(train_loader):
+        log_losses = i > 0 and n_iter % args.print_freq == 0
+        log_output = args.training_output_freq > 0 and n_iter % args.training_output_freq == 0
+
+        # measure data loading time
+        data_time.update(time.time() - end)
+        img_ref = img_ref.to(device)
+        img_tar = img_tar.to(device)
+
+        # compute output
+        _, poses_pred = disvo(img_ref, img_tar)
+
+        loss = sum((poses_pred[:6] - poses_gt).^2 * torch.exp(-poses_pred[6:]) + poses[6:])
+
+        if log_losses:
+            train_writer.add_scalar('total_loss', loss.item(), n_iter)
+
+        if log_output:
+            train_writer.add_image('train Input', tensor2array(tgt_img[0]), n_iter)
+            for k, scaled_maps in enumerate(zip(depth, disparities, warped, diff, explainability_mask)):
+                log_output_tensorboard(train_writer, "train", k, n_iter, *scaled_maps)
+
+        # record loss and EPE
+        losses.update(loss.item(), args.batch_size)
+
+        # compute gradient and do Adam step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        with open(args.save_path/args.log_full, 'a') as csvfile:
+            writer = csv.writer(csvfile, delimiter='\t')
+            writer.writerow([loss.item()])
+        logger.train_bar.update(i+1)
+        if i % args.print_freq == 0:
+            logger.train_writer.write('Train: Time {} Data {} Loss {}'.format(batch_time, data_time, losses))
+        if i >= epoch_size - 1:
+            break
+
+        n_iter += 1
+
+    return losses.avg[0]
+
+
+@torch.no_grad()
+def validate(args, val_loader, disvo, 0, logger, output_writers):
+    global device
+    batch_time = AverageMeter()
+    error_names = ['abs_diff', 'abs_rel', 'sq_rel', 'a1', 'a2', 'a3']
+    errors = AverageMeter(i=len(error_names))
+    log_outputs = len(output_writers) > 0
+
+    # switch to evaluate mode
+    disp_net.eval()
+
+    end = time.time()
+    logger.valid_bar.update(0)
+    for i, (tgt_img, depth) in enumerate(val_loader):
+        tgt_img = tgt_img.to(device)
+        depth = depth.to(device)
+
+        # compute output
+        output_disp = disp_net(tgt_img)
+        output_depth = 1/output_disp[:,0]
+
+        if log_outputs and i < len(output_writers):
+            if epoch == 0:
+                output_writers[i].add_image('val Input', tensor2array(tgt_img[0]), 0)
+                depth_to_show = depth[0]
+                output_writers[i].add_image('val target Depth',
+                                            tensor2array(depth_to_show, max_value=10),
+                                            epoch)
+                depth_to_show[depth_to_show == 0] = 1000
+                disp_to_show = (1/depth_to_show).clamp(0,10)
+                output_writers[i].add_image('val target Disparity Normalized',
+                                            tensor2array(disp_to_show, max_value=None, colormap='magma'),
+                                            epoch)
+
+            output_writers[i].add_image('val Dispnet Output Normalized',
+                                        tensor2array(output_disp[0], max_value=None, colormap='magma'),
+                                        epoch)
+            output_writers[i].add_image('val Depth Output',
+                                        tensor2array(output_depth[0], max_value=10),
+                                        epoch)
+
+        errors.update(compute_errors(depth, output_depth))
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+        logger.valid_bar.update(i+1)
+        if i % args.print_freq == 0:
+            logger.valid_writer.write('valid: Time {} Abs Error {:.4f} ({:.4f})'.format(batch_time, errors.val[0], errors.avg[0]))
+    logger.valid_bar.update(len(val_loader))
+    return errors.avg, error_names
+
+
+if __name__ == '__main__':
+    main()
